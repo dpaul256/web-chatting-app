f5ffae873a4d7433db3b6cc0c1fa91a2646d428d
Zhong Bo-Hsun <jone921216@gmail.com>
2026-04-13 17:40:20 +0800

    adding log in system

--- vite.config.js
+++ vite.config.js
@@ -1,5 +1,14 @@
 import { defineConfig } from 'vite'
+import { resolve } from 'path'
 
 export default defineConfig({
-  base: '/web-chatting-app/'
+  base: '/web-chatting-app/', 
+  build: {
+    rollupOptions: {
+      input: {
+        main: resolve(__dirname, 'index.html'),
+        chat: resolve(__dirname, 'chat.html'),
+      },
+    },
+  },
 })
